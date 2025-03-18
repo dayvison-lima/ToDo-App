11ac9c7f7724c48db9744fe5c874cb23eb25e895
Dayvison Lima <limaare@outlook.com>
2025-03-18 15:43:13 -0300

    permitindo que o front acesse o back

--- backend/meu_projeto/settings.py
+++ backend/meu_projeto/settings.py
@@ -73,8 +73,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -153,7 +153,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Permite que o Next.js acesse a API
+    "https://todo-app-1-4ae3.onrender.com/",  # Permite que o Next.js acesse a API
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://todo-app-1-4ae3.onrender.com",  # ✅ Permite requisições seguras
 ]
 
 CORS_ALLOW_CREDENTIALS = True
